f5605caa1adf706536f8adb43ba4e58ae476feed
Marcin890 <m.koska2@wp.pl>
2020-01-06 18:12:05 +0100

    Improve intro and forms

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,10 +1,10 @@
-import gsap from "gsap";
-import { TimelineMax } from "gsap/all.js";
-
+import { TimelineMax } from "gsap";
+import { gsap } from "gsap";
+gsap.registerPlugin(TimelineMax);
 const show = () => {
   const tl = new TimelineMax({ repeat: 0 });
   const ornament = document.querySelectorAll(".intro__wrapper");
-  tl.set(ornament, { opacity: 0 }).staggerTo(ornament, 2.5, {
+  tl.set(ornament, { opacity: 0 }).staggerTo(ornament, 1.5, {
     opacity: 1
   });
   return tl;
@@ -19,9 +19,39 @@ const rotation = () => {
   return tl;
 };
 
+const button = () => {
+  const tl = new TimelineMax({ repeat: -1, yoyo: true });
+  const button = document.querySelector(".intro__button");
+  tl.to(button, 1.2, { scale: 0.9 });
+  return tl;
+};
+
+const dots = () => {
+  const tl = new TimelineMax({ repeat: -1, yoyo: false });
+  const dots = document.querySelectorAll(".intro__outerCircle-item");
+  const color = () => {
+    const colors = ["#808000", "#FFA500", "#663399"];
+    return colors[Math.floor(Math.random() * 3)];
+  };
+  tl.staggerTo(
+    dots,
+    1,
+    {
+      stroke: color
+    },
+    0.5
+  );
+
+  return tl;
+};
+
 const main = new TimelineMax();
+
 main.add(show());
-main.add(rotation());
+main.add("start");
+main.add(dots(), "start");
+main.add(rotation(), "start");
+main.add(button(), "start");
 
 const intro = document.querySelector(".intro");
 intro.addEventListener("click", () => {
--- src/js/showMenu.js
+++ src/js/showMenu.js
@@ -1,6 +1,7 @@
 function showMenu(e) {
   e.preventDefault();
   var x = document.querySelector(".nav__wrapper");
+
   if (x.className === "nav__wrapper") {
     x.className += " nav__responsive";
     console.log("ok");
